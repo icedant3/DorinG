0ab9239cc0e623a426c963b7b5b40c3b6c0709cb
KenJi <dan.baltag.9878@gmail.com>
2017-11-12 14:18:01 +0200

    corectat

--- Makefile
+++ Makefile
@@ -1,4 +1,11 @@
-all:main.cpp lab2.cpp lab2.hpp
-	g++ *.cpp
+OB=main.o lab2.o
+
+all: $(OB)
+	g++ $(OB)
 	./a.out
 
+main.o: main.cpp
+	g++ -c main.cpp
+
+lab2.o: lab2.cpp
+	g++ -c lab2.cpp
--- main.cpp
+++ main.cpp
@@ -4,5 +4,5 @@ int main(){
 	piramida a(5);
 	piramida b(8);
 	
-	cout<<a.getvolume()>b.getvolume()?a.getvolume():b.getvolume()<<"\n";
+	cout<<(a.getvolume()>b.getvolume()?a.getvolume():b.getvolume())<<"\n";
 }
