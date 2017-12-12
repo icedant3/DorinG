9f07ed1cd0c735b7348bc2cfeac3a80dc894a409
icedant3 <33224293+icedant3@users.noreply.github.com>
2017-12-12 20:11:18 +0200

    Add files via upload

---
+++ LAB3.hpp
@@ -0,0 +1,30 @@
+#ifndef Polinom_h
+#define Polinom_h
+
+class Polinom
+{
+	int grad;
+	int *coef;
+public :
+	Polinom(int g=0); //constructor implicit
+	~Polinom (); // destructor implicit
+	Polinom (int , int *); //constructor
+	Polinom ( const Polinom&);//constructor de copiere
+
+	void afisare (char * ); // afisarea unui polinom   OUTPUT FOR POLYNOM
+	int valPolinom (int); // valoarea unui polinom intr-un punct
+
+	Polinom operator + ( Polinom &);// adunarea polinoamelor
+	friend Polinom operator = ( Polinom & , Polinom &);// atribuirea polinoamelor
+	Polinom operator * ( Polinom &);// inmultirea polinoamelor
+
+};
+#endif
+
+
+
+
+
+
+
+
